04fb94544b124327544ca81f3717ff60985c937b
Andrey Pechennikov <ap@devandy.me>
2022-08-27 22:34:21 +0300

    Добавил рандомное проставление статуса выполненной задачи

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ $show_complete_tasks = rand(0, 1);
                         <input
                             class="checkbox__input visually-hidden show_completed"
                             type="checkbox"
-                            <?= $show_complete_tasks === 1 ? "checked" : "" ?>
+                            <?= $show_complete_tasks ? "checked" : "" ?>
                         >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
@@ -97,7 +97,7 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-                    <?php if($show_complete_tasks === 1): ?>
+                    <?php if($show_complete_tasks): ?>
                         <tr class="tasks__item task task--completed">
                             <td class="task__select">
                                 <label class="checkbox task__checkbox">
