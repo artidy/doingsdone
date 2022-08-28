ec8cc0eddc4f03bcb07c2b82ca417af6304818ce
Andrey Pechennikov <ap@devandy.me>
2022-08-28 17:40:37 +0300

    Добавил в сценарий главной страницы подключение к базе

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
-require_once("config.php");
-require_once("helpers.php");
-require_once("data.php");
-
 /**
+ * Сценарий главной страницы
  * @var $projects array<string> - массив проектов
  * @var $tasks array<array{title:string, deadline:string, project:string, is_complited:bool}> - массив задач по проектам
  * @var $show_complete_tasks bool - статус отображения выполненных задач
  */
+
+require_once("config.php");
+require_once("db.php");
+require_once("helpers.php");
+require_once("data.php");
+
 $main_template = include_template("main.php", [
     "projects" => $projects,
     "tasks" => $tasks,
